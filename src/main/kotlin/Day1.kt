ea73f92a66ec19950d310d62214a599cdd4251a9
Przemyslaw Materna <przemyslaw.materna@ocado.com>
2022-12-01 16:07:31 +0100

    day1

---
+++ src/main/kotlin/Day1.kt
@@ -0,0 +1,26 @@
+@Challenge(1)
+class Day1 : BaseChallenge<List<Int?>>() {
+    override val inputPath: String = "input/day1.txt"
+
+    override fun parse(input: List<String>): List<Int?> {
+        return input.map { it.toIntOrNull() }
+    }
+
+    override fun ex1(): String {
+        return input
+            .runningReduce { acc, i -> if (i == null) 0 else acc!! + i }
+            .mapNotNull { it }
+            .maxBy { it }
+            .toString()
+    }
+
+    override fun ex2(): String {
+        return input
+            .runningReduce { acc, i -> if (i == null) 0 else acc!! + i }
+            .mapNotNull { it }
+            .sorted()
+            .takeLast(3)
+            .sum()
+            .toString()
+    }
+}
